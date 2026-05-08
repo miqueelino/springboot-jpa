0b8832f015fdad56c07bf6f854a1d83aeac89606
miqueelino <jpaulo.nunez@gmail.com>
2026-05-08 16:45:38 -0300

    Project created

---
+++ src/main/java/com/exeweb/ServiceJPA/ServiceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.exeweb.ServiceJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exeweb/ServiceJPA/ServiceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exeweb.ServiceJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
